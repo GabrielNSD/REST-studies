2890a3d44315c606e37d300a932c846b76c892bd
gabrielnsd <gabrielnsdantas@gmail.com>
2021-06-13 11:45:36 -0300

    express-ts implementation

--- Go/cmd/main.go
+++ Go/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,8 +14,16 @@ func main() {
 
 	router := chi.NewRouter()
 	router.Get("/api/get", getHandler)
+	router.Post("/api/post", postHandler)
+
+	fmt.Println("Server is listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Inside GET")
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Post req")
 }
